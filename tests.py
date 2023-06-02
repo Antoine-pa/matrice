ab5176b5a13b401e05f1525b047fbd1504827057
dBasdk <modenosino@gmail.com>
2023-06-02 12:53:17 +0200

    final commit

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
 from matrice import Matrice
+import timeit
 
 m = Matrice([[1, 1, 2], [3, 4, 5], [6, 7, 8]])
 m2=Matrice([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-print(m.det())
-m[(0, 0)] = 2
-print(m.det())
+
+print(m)
+print(m.comatrice())
+print(m.comatrice2())
+
+print(timeit.timeit("m.comatrice()", "from __main__ import m", number=10000))
